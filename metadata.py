c30042c2609ff65428d5d924fd76223e53c3aaa1
AbsCtrl <71909576+AbsCtrl@users.noreply.github.com>
2024-08-01 20:30:09 -0400

    Added metadata

---
+++ metadata.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.1"
+version = __version__
+title = "sample-fastAPI"
+description = """
+API to showcase the fast-API framework
+"""
+contact = {"name": "Vincent", "email": "vincent.yang6237@gmail.com"}
+tags_metadata = [{"name": "health", "description": "report on service health"}]
